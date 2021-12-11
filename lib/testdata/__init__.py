bb8b3af068e831c0420cd687129b14b733adf408
Shahruk Hossain <shahruk10@gmail.com>
2021-12-12 04:08:54 +0600

    test(lib/models): unit tests and reference data for kaldi Sequential models
      - Added reference input and outputs for two kadli x-vector models.
    
      - Added unit tests that check the cosine distance between the reference
        x-vector and the one produced using the tensorflow model.
    
      - Added script used to generate the reference outputs from particular
        layers in the model.

--- lib/testdata/__init__.py
+++ lib/testdata/__init__.py
@@ -9,5 +9,6 @@ from .xvectors.xvectors import RefXVectors
 
 from .tdnn.tdnn_narrow import RefTdnnNarrow
 from .tdnn.tdnn_single_layer import RefTdnnSingleLayer
+from .tdnn.tdnn_models import RefKaldiXVectorModels
 
 from .stats.stats_pooling import RefStatsPooling
