198825975241a2c306936e3add8ab2051c6c0c0e
Shahruk Hossain <shahruk10@gmail.com>
2021-12-12 03:59:24 +0600

    feat(lib/models): build kaldi models from config file
      - The kaldi models using TDNN, ReLU, BatchNorm and StatPooling
        layers can now be build from yml config files specifying the layers.
    
      - These models can then be intiialized with weights from kaldi's nnet3
        model files.
    
      - Added two example config files for x-vector models available on the
        kaldi website.
    
      - Added pyyaml and tqdm to dependencies.

---
+++ lib/models/kaldi/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+
+from .sequential import SequentialModel
---
+++ lib/models/kaldi/sequential.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+
+from typing import Iterable
+
+from tensorflow.keras.layers import Layer, Input, ReLU
+from tensorflow.keras.models import Sequential
+
+from lib.layers import TDNN, BatchNorm, StatsPooling
+from lib.io import KaldiNnet3Reader
+
+
+def cfg2layers(layerCfg: dict) -> Iterable[Layer]:
+    """
+    Uses the given layer config to instantiate one or more
+    tensorflow layers.
+
+    Parameters
+    ----------
+    layerCfg : dict
+        Layer config. May specify multiple layers by specifying
+        a list of layer types, e.g. ["affine", "relu", "batchnorm"].
+        See ${TOP}/data/kaldi_models/configs/ for examples.
+
+    Returns
+    -------
+    Iterable[Layer]
+        One or more layers instantiated from the layer config.
+
+    Raises
+    ------
+    KeyError
+        If the layer config is missing necessary properties.
+
+    ValueError
+        If the type specified in the layer config is not supported.
+    """
+
+    layerTypes = layerCfg.get("type", [])
+    if isinstance(layerTypes, str):
+        layerTypes = [layerTypes]
+    if len(layerTypes) == 0:
+        raise KeyError("layer config does not define layer 'type'")
+
+    name = layerCfg.get("name", None)
+
+    layers = []
+    for layerType in layerTypes:
+        t = layerType.lower()
+        cfg = layerCfg.get("cfg", {})
+
+        if t in ["affine", "tdnn"]:
+            cfg["name"] = f"{name}.affine"
+            layer = TDNN(**cfg)
+        elif t in ["relu"]:
+            layer = ReLU(name=f"{name}.relu")
+        elif t in ["batchnorm", "bn"]:
+            layer = BatchNorm(name=f"{name}.batchnorm")
+        elif t in ["stats", "stats_extraction", "stats_pooling"]:
+            cfg["name"] = name
+            layer = StatsPooling(**cfg)
+        else:
+            raise ValueError(f"unsupported layer type '{t}'")
+
+        layers.append(layer)
+
+    return layers
+
+
+def SequentialModel(cfg: dict, nnet3Path: str = None) -> Sequential:
+    """
+    Creates a tensorflow.keras.Sequential model using the given configuration.
+
+    Parameters
+    ----------
+    cfg : dict
+        Model config. See ${TOP}/data/kaldi_models/configs/ for examples.
+
+    Returns
+    -------
+    Sequential
+        Model created using the config.
+
+    nnet3Path : str, optional
+        If path to nnet3 raw file provided, the created tensorflow model
+        will be initialized using weights from nnet3 model. By default None.
+
+    Raises
+    ------
+    ValueError
+        If config is missing layer configs.
+        If first layer in layer config is not an input layer.
+    """
+    layersConfig = cfg.get("layers", [])
+    if len(layersConfig) == 0:
+        raise ValueError("no layers defined in config")
+
+    layers = []
+
+    # First layer should be input.
+    inputCfg = layersConfig[0]
+    if inputCfg.get("type", "") != "input":
+        raise ValueError("first layer in sequential model needs to be of type 'input'")
+
+    batchSize, timesteps, featDim = inputCfg["shape"]
+    layers.append(Input(shape=(timesteps, featDim), batch_size=batchSize))
+
+    # Creating rest of the layers.
+    for lCfg in cfg["layers"][1:]:
+        layers.extend(cfg2layers(lCfg))
+
+    mdl = Sequential(layers)
+
+    # Initializing weights if path to nnet3 model given.
+    if nnet3Path is not None:
+        nnet3Mdl = KaldiNnet3Reader(nnet3Path, True)
+        for layer in mdl.layers:
+            try:
+                layer.set_weights(nnet3Mdl.getWeights(layer.name))
+            except KeyError:
+                print(f"component with name '{layer.name}' not found in nnet3 model, "
+                      "skipping initialization")
+
+    return mdl
